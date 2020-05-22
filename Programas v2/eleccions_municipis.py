02e048d7741db84540ad47648533fdcd5cf58c71
bbddeleccionesgenerales <62172055+bbddeleccionesgenerales@users.noreply.github.com>
2020-05-22 15:15:15 +0200

    Manu

--- Programas v2/eleccions_municipis.py
+++ Programas v2/eleccions_municipis.py
@@ -51,10 +51,10 @@ try: # Creacion de excepcion
             llista.append(linea[205:213])# Votos candidaturas
             llista.append(linea[189:197])# Votos blanco
             llista.append(linea[197:205])# Votos nulos
-            insert_comunitats_autonomes = ('INSERT INTO eleccions_municipis (eleccio_id, codi_ine_municipi, num_meses, '
+            insert_eleccions_municipis = ('INSERT INTO eleccions_municipis (eleccio_id, codi_ine_municipi, num_meses, '
                                            'poblacio, cens, vots_candidatures, vots_blanc, vots_nuls) '
                                            'VALUES (1, %s, %s, %s, %s, %s, %s)')
-            cursor.execute(insert_comunitats_autonomes, llista) # Insertamos en tabla de BBDD
+            cursor.execute(insert_eleccions_municipis, llista) # Insertamos en tabla de BBDD
 except OSError as e: # Resolucion excepcion
     print('Imposible abrir fichero ' + pathFitxer)
 
